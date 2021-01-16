d831f60ec2afef5fc5c108bed861707378b871ec
Vasileva Alena <Vasileva-25@mail.ru>
2021-01-16 21:54:04 +0300

    add change vk_general_friends.py

--- vk_general_friends.py
+++ vk_general_friends.py
@@ -6,34 +6,29 @@ token = input('Введите ваш токен: ')
 class VkFriends:
     url = 'https://api.vk.com/method/'
 
-    def __init__(self, user_id, version):
+    def __init__(self, user_id):
         self.user_id = user_id
-        self.version = version
         self.params = {
             'access_token': token,
-            'v': self.version
+            'v': '5.126'
         }
 
-    def get_friends(self):
-        friends_url = self.url + 'friends.get'
+    def __and__(self, other_user):
+        friends_url = self.url + 'friends.getMutual'
         friends_params = {
-            'user_id': self.user_id,
-            'fields': 'domain'
+            'source_uid': self.user_id,
+            'target_uid': other_user.user_id
         }
         res = requests.get(friends_url, params={**self.params, **friends_params})
-        list_friends = []
-        info_friends = res.json()['response']['items']
+        mutal_user_lust = res.json()['response']
+        return mutal_user_lust
 
-        for info_friend in info_friends:
-            profile_friends = ('https://vk.com/' + info_friend['domain'])
-            list_friends.append(profile_friends)
-        return list_friends
+    def __str__(self):
+        print('https://vk.com/id' + self.user_id)
 
 
-user1 = VkFriends('154010681', '5.126')
-user2 = VkFriends('29414115', '5.126')
-user1_friends = set(user1.get_friends())
-user2_friends = set(user2.get_friends())
-general_friends = user1_friends & user2_friends
-
-print(f'У user1 и user2 общих друзей - {len(general_friends)}. \n Ссылки на их профиль: \n {general_friends}')
+user1 = VkFriends('154010681')
+user2 = VkFriends('29414115')
+print(user1 & user2)
+user1.__str__()
+user2.__str__()
